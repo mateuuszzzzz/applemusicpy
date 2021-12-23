f7509894de415c084094f5b3d84b5543d48da288
mateuuszzzzz <mati.icloud2000@gmail.com>
2021-12-23 22:45:53 +0100

    Changed name of an example

---
+++ examples/custom_request.py
@@ -0,0 +1,17 @@
+import applemusicpy
+
+team_id = 'team_id'
+key_id = 'key_id'
+secret_file = 'AuthKey_' + key_id + '.p8'
+
+with open(secret_file,'r') as f:
+    secret_key = f.read()
+
+auth = applemusicpy.Auth(secret_key=secret_key, key_id=key_id, team_id=team_id)
+client = applemusicpy.Client(auth=auth)
+
+params = {
+    'limit': 3
+}
+
+print(client('GET', 'https://api.music.apple.com/v1/me/library/albums', params, 'library'))
